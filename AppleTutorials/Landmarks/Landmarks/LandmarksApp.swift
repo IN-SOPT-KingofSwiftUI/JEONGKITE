0472ac8877aaf31d1ca2baf8b9cdfc9c0e2f892f
jeongkite <jeong6kite@gmail.com>
2022-10-12 11:47:39 +0900

    [#1] Create a New Project

---
+++ AppleTutorials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 양정연 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
