b9f7455e7a175622433c536ae2d286db5b25b21b
jeongkite <jeong6kite@gmail.com>
2022-10-13 22:01:19 +0900

    [#3] Create the List of Landmarks

---
+++ AppleTutorials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 양정연 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
